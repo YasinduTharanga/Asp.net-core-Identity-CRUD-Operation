66b5665bd71de922c9ea9a430978b1633fd1a85d
yasindutharnga <ypremathilaka@corporate.mediadefined.com>
2024-12-12 15:49:24 +0530

    Add project files.

---
+++ CRUD_Operations/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRUD_Operations.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_Operations.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
